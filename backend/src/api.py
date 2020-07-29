30d4fdff4c443ad464a7b26e698e56a243ebe2da
Mai Campbell <jirallc@gmail.com>
2020-07-29 15:54:06 -0400

    feat: implement delete and patch methods

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 @app.route('/drinks')
@@ -33,29 +33,18 @@ def show_drinks():
 
 @app.route('/drinks-detail')
 @requires_auth('get:drinks-detail')
-def show_drink_detail(token):
+def drink_detail(token):
     try:
-        return json.dumps({
-            'success':
-            True,
-            'drinks': [drink.long() for drink in Drink.query.all()]
+        available_drink = Drink.query.all()
+
+        drinks = [drink.long() for drink in available_drink]
+        print(drinks)
+        return jsonify({
+            'success': True,
+            'drinks': [drinks]
         }), 200
     except:
-        return json.dumps({
-            'success': False,
-            'error': "An error occurred"
-        }), 500
-    
-
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+        abort(404)
 
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
@@ -77,24 +66,28 @@ def new_drink(token):
     except:
         abort(422)
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
-# @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
-# @requires_auth('patch:drinks')
-# def update_drink(token, drink_id):
-#     try:
-#         data = request.get_json()
-
-
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(token, drink_id):
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+    if drink:
+        try:
+            data = request.get_json()
+            update_title = data.get('title', None)
+            update_recipe = data.get('recipe', None)
+            if update_title:
+                drink.title = update_title
+            if update_recipe:
+                drink_recipe = update_recipe
+            drink.update()
+            return json.dumps({
+                'success': True,
+                'drinks': [drink.long()]
+            })
+        except:
+            abort(422)
+    else:
+        abort(404)
 
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
@@ -113,10 +106,10 @@ def delete_drink(token, drink_id):
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+        "success": False, 
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
 
 @app.errorhandler(404)
 def not_found(error):
@@ -130,6 +123,6 @@ def not_found(error):
 def auth_error(error):
     return jsonify({
         'success': False,
-        'message': error.status_code,
-        'error': error.error
-    }), 401
+        'message': 'Error with authorization',
+        'error': AuthError
+    }), AuthError
